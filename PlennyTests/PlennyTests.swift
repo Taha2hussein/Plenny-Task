badbea3574af5006b25fcaf61999d16e3031aca8
Taha Hussein <taha.hussein.two@gmail.com>
2023-09-19 19:37:43 +0300

    add unit testing

--- PlennyTests/PlennyTests.swift
+++ PlennyTests/PlennyTests.swift
@@ -6,31 +6,77 @@
 //
 
 import XCTest
+import Combine
+import SwiftUI
 @testable import Plenny
 
 final class PlennyTests: XCTestCase {
-
+    
+    var subscriptions = Set<AnyCancellable>()
+     var viewModel: LoginViewModel!
+    private var disposal = Disposal()
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = LoginViewModel()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testLogin() {
+        let expection = XCTestExpectation(description: "login")
+        let reporsitry = viewModel.repositry
+        var loginResponse: LoginModel?
+        var errorResponse: Error?
+        reporsitry.login(username: "kminchelle", password: "0lelplR").observe { result, _ in
+            guard let result = result else { return }
+            
+            switch result {
+            case .success(let sucess):
+                loginResponse = sucess
+            case .failure(let error):
+                errorResponse = error
+                
+            }
+            expection.fulfill()
+        }.add(to: &disposal)
+        wait(for: [expection], timeout: 3.0)
+        XCTAssertNil(errorResponse)
+        XCTAssertNotNil(loginResponse)
+        
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testPosts() {
+        let expection = XCTestExpectation(description: "posts downloaded")
+        let reporsitry = viewModel.repositry
+         var posts = [Post]()
+        var errorResponse: Error?
+        reporsitry.getAllPosts().observe { result, _ in
+            guard let result = result else { return }
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let resonse):
+                    posts = resonse.posts
+                case .failure(let error):
+                    errorResponse = error
+                }
+            }
+            expection.fulfill()
+        }.add(to: &disposal)
+        wait(for: [expection], timeout: 5.0)
+        XCTAssertNil(errorResponse)
+        XCTAssertNotNil(posts)
+        let postsCount = posts.count
+        XCTAssertEqual(postsCount, 30, "Posts count should be more that 0")
     }
-
 }
